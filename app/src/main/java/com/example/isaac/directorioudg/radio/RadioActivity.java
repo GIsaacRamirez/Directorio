474a825ae9dc8eb21d4f00cc26988cece1e27733
isaac <guillermomembri@gmail.com>
2016-10-03 12:07:59 -0600

    Se agrego la peticion de permisos del telefono

--- app/src/main/java/com/example/isaac/directorioudg/radio/RadioActivity.java
+++ app/src/main/java/com/example/isaac/directorioudg/radio/RadioActivity.java
@@ -1,12 +1,17 @@
 package com.example.isaac.directorioudg.radio;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -31,6 +36,7 @@ public class RadioActivity extends AppCompatActivity implements MediaPlayer.OnPr
     int image;
     Helper helper = new Helper(this);
     View view;
+    private static final int PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
     public String URL;
 
     @Bind(R.id.imageVolume)
@@ -103,6 +109,34 @@ public class RadioActivity extends AppCompatActivity implements MediaPlayer.OnPr
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+        //Pedir permiso de lectura
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.READ_PHONE_STATE}, PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+            return;
+        }
+    }
+
+    //Respuesta de los permisos
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    showSnackbar("Es necesario el permiso para detener la reproduccion al recibir llamadas");
+                }
+            }
+        }
     }
 
     @Override
@@ -176,6 +210,9 @@ public class RadioActivity extends AppCompatActivity implements MediaPlayer.OnPr
         MusicService.resetPlayer(this);
     }
 
+    private void showSnackbar(String msg) {
+        Snackbar.make(getWindow().findViewById(android.R.id.content), msg, Snackbar.LENGTH_SHORT).show();
+    }
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
         progressBar3.setVisibility(View.GONE);
