cf13a683e2bcb71e46da8b07e7c1b1b8da29aca2
isaac <guillermomembri@gmail.com>
2016-10-04 14:21:24 -0500

    Se puede comnpartir la imagen

--- app/src/main/java/com/example/isaac/directorioudg/zoom.java
+++ app/src/main/java/com/example/isaac/directorioudg/zoom.java
@@ -1,13 +1,24 @@
 package com.example.isaac.directorioudg;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.isaac.directorioudg.lib.GlideImageLoader;
 import com.example.isaac.directorioudg.lib.ImageLoader;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,7 +29,8 @@ public class zoom extends AppCompatActivity {
     @Bind(R.id.imagen_extendida)
     ImageViewTouch imagenExtendida;
 
-    ImageLoader imageLoader;
+    @Bind(R.id.image_paralax)
+    ImageView imageParalax;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
@@ -29,28 +41,41 @@ public class zoom extends AppCompatActivity {
         setContentView(R.layout.activity_zoom);
         ButterKnife.bind(this);
         setToolbar();
-        imageLoader = new GlideImageLoader(this.getApplicationContext());
         Bundle bundle = this.getIntent().getExtras();
         String url = bundle.getString("url");
-
         loadImage(url);
     }
 
     private void loadImage(String url) {
-
-
         if (url.equalsIgnoreCase("No Disponible")) {
             Glide.with(getApplicationContext())
                     .load(R.drawable.fotolugarvacio)
                     .fitCenter()
                     .into(imagenExtendida);
+            Glide.with(getApplicationContext())
+                    .load(R.drawable.fotolugarvacio)
+                    .fitCenter()
+                    .into(imageParalax);
         } else {
             Glide.with(getApplicationContext())
                     .load(url.toString())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .fitCenter()
                     .into(imagenExtendida);
+            Glide.with(getApplicationContext())
+                    .load(url.toString())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .fitCenter()
+                    .into(imageParalax);
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_zoom, menu);
+        return true;
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -58,11 +83,39 @@ public class zoom extends AppCompatActivity {
             case android.R.id.home:
                 onBackPressed();
                 return true;
+            case R.id.action_compartir:
+                shareImage();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    private void shareImage(){
+        imageParalax.buildDrawingCache();
+        Bitmap bitmap = imageParalax.getDrawingCache();
+        /***** COMPARTIR IMAGEN *****/
+        try {
+            File file = new File(imageParalax.getContext().getCacheDir(), bitmap + ".jpg");
+            FileOutputStream fOut = null;
+            fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            file.setReadable(true, false);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.setType("image/jpg");
+            startActivity(Intent.createChooser(intent, "Compartir"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            showSnackbar("Error por favor intente de nuevo");
+        }
+    }
+    private void showSnackbar(String msg) {
+        Snackbar.make(getWindow().findViewById(android.R.id.content), msg, Snackbar.LENGTH_SHORT).show();
+    }
     private void setToolbar() {
         // Añadir la Toolbar
         setSupportActionBar(toolbar);
